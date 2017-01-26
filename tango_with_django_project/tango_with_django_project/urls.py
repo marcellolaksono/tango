eef5203ac0faf68fcdbd1d74537153d860e2f470
Marcello Laksono <2204494l@student.gla.ac.uk>
2017-01-26 19:01:36 +0000

    update2

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include 
+from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
