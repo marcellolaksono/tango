38c928382a5b8036e8d3c83bb50741f9f768344c
Marcello Laksono <2204494l@student.gla.ac.uk>
2017-02-09 17:26:01 +0000

    chap6 exercise

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,65 +1,79 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                       'tango_with_django_project.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 import django
+
 django.setup()
 from rango.models import Category, Page
+
+
 def populate():
-    python_cat = add_cat('Python', views=128, likes=64)
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 10},
+        {"title": "How to Think like a Computer Scientist",
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 23},
+        {"title": "Learn Python in 10 Minutes",
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 34}]
 
-    add_page(cat=python_cat,
-        title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+    django_pages = [
+        {"title": "Official Django Tutorial",
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 22},
+        {"title": "Django Rocks",
+         "url": "http://www.djangorocks.com/",
+         "views": 11},
+        {"title": "How to Tango with Django",
+         "url": "http://www.tangowithdjango.com/",
+         "views": 48}]
 
-    add_page(cat=python_cat,
-        title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+    other_pages = [
+        {"title": "Bottle",
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 7},
+        {"title": "Flask",
+         "url": "http://flask.pocoo.org",
+         "views": 2}]
 
-    add_page(cat=python_cat,
-        title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+    cats = {"Python": {"pages": python_pages},
+            "Django": {"pages": django_pages},
+            "Other Frameworks": {"pages": other_pages}}
 
-    django_cat = add_cat("Django", views=64, likes=32)
+    i = 128
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, i, i / 2)
+        i = i / 2
 
-    add_page(cat=django_cat,
-        title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"], p["views"])
 
-    add_page(cat=django_cat,
-        title="Django Rocks",
-        url="http://www.djangorocks.com/")
-
-    add_page(cat=django_cat,
-        title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
-
-    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
-
-    add_page(cat=frame_cat,
-        title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
-
-    add_page(cat=frame_cat,
-        title="Flask",
-        url="http://flask.pocoo.org")
-
-    # Print out what we have added to the user.
+    # Print out the categories we have added
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print "- {0} - {1}".format(str(c), str(p))
+            print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url = url
+    p.views = views
+    p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save(0)
     return c
+
 
 # Start execution here!
 if __name__ == '__main__':
-    print "Starting Rango population script..."
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-    from rango.models import Category, Page
+    print("Starting Rango population script...")
     populate()
